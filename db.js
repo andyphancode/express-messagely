33123e1bd212de19a3903c4a8a4ab14c020358fb
andyphancode <andyphanpxh@gmail.com>
2023-05-24 04:30:32 -0700

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,10 @@
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
-const client = new Client(DB_URI);
+const client = new Client({
+    host: "/var/run/postgresql/",
+    database: DB_URI    
+});
 
 client.connect();
 
